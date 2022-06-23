11d2d50eec718125d8513cc134f09732c5ace3c0
Xie Haoxuan <119010351@link.cuhk.edu.cn>
2022-06-23 10:19:51 +0000

    Remove unused modules

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ if [ -n "$1" ]; then
     ./main graph.txt query.txt output.txt $1
 else
     ./main graph.txt query.txt output_online.txt Online > log_online.txt
+    ./main graph.txt query.txt output_baseline.txt Baseline > log_baseline.txt
 fi
