5988d2cfe3923f0b0aa6601b2c0f2ed9d3cebc0c
Xie Haoxuan <119010351@link.cuhk.edu.cn>
2022-10-08 03:24:29 +0000

    update datasets

--- graph-gen.py
+++ graph-gen.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     DATASETS_URL = ["http://konect.cc/files/download.tsv.dblp-cite.tar.bz2",
                     "http://konect.cc/files/download.tsv.flickr-growth.tar.bz2",
                     "http://konect.cc/files/download.tsv.wikipedia-growth.tar.bz2",
-                    "http://snap.stanford.edu/data/sx-mathoverflow-c2a.txt.gz",
+                    "http://snap.stanford.edu/data/sx-mathoverflow-a2q.txt.gz",
                     "https://snap.stanford.edu/data/email-Eu-core-temporal.txt.gz",
                     "https://snap.stanford.edu/data/CollegeMsg.txt.gz"]
     if os.path.isdir("datasets") is False or len(os.listdir("datasets")) < len(DATASETS_URL):
