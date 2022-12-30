726ef8fdb8da01b751c3debdcb35e002644c2c29
Xie Haoxuan <119010351@link.cuhk.edu.cn>
2022-12-30 07:11:35 +0000

    Add support multiple query files

--- source/main.cpp
+++ source/main.cpp
@@ -24,24 +24,25 @@ int main(int argc, char * argv[]) {
     unsigned long long start_time = currentTime();
 
     if (argc != 5) {
-        std::cout << "Parameter error. Please check the readme file." << std::endl;
-    	return 0;
+        std::cout << "Parameters are non-standard. Please check the readme file." << std::endl;
     }
 
     TemporalGraph * Graph = build(argv);
     // Graph->shrink_to_fit();
     int vertex_num = Graph->numOfVertices();
 
-    if (std::strcmp(argv[4], "Online") == 0) {
-        std::cout << "Running online search..." << std::endl;
-        unsigned long long online_search_start_time = currentTime();
-        online(Graph, argv[2], argv[3]);
-        unsigned long long online_search_end_time = currentTime();
-        std::cout << "Online search completed in " << timeFormatting(online_search_end_time - online_search_start_time).str() << std::endl;
+    if (std::strcmp(argv[argc - 1], "Online") == 0) {
+        for (int i = 2; i < argc - 2; i++) {
+            std::cout << "Running online search..." << std::endl;
+            unsigned long long online_search_start_time = currentTime();
+            online(Graph, argv[i], argv[argc - 2]);
+            unsigned long long online_search_end_time = currentTime();
+            std::cout << "Online search completed in " << timeFormatting(online_search_end_time - online_search_start_time).str() << std::endl;
+        }
         delete Graph;
     }
 
-    if (std::strcmp(argv[4], "Baseline") == 0) {
+    if (std::strcmp(argv[argc - 1], "Baseline") == 0) {
         std::cout << "Running baseline..." << std::endl;
         std::cout << "Constructing the index structure..." << std::endl;
         unsigned long long index_construction_start_time = currentTime();
@@ -50,31 +51,33 @@ int main(int argc, char * argv[]) {
         std::cout << "Index construction completed in " << timeFormatting(index_construction_end_time - index_construction_start_time).str() << std::endl;
         std::cout << "Index cost " << Index->size() << " bytes" << std::endl;
         delete Graph;
-        std::cout << "Solving queries..." << std::endl;
-        unsigned long long query_start_time = currentTime();
-        baseline(Index, vertex_num, argv[2], argv[3]);
-        unsigned long long query_end_time = currentTime();
-        std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+        for (int i = 2; i < argc - 2; i++) {
+            std::cout << "Solving queries..." << std::endl;
+            unsigned long long query_start_time = currentTime();
+                baseline(Index, vertex_num, argv[i], argv[argc - 2]);
+            unsigned long long query_end_time = currentTime();
+            std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+        }
         std::cout << "Baseline completed!" << std::endl;
     }
 
-    if (std::strcmp(argv[4], "DC") == 0) {
-        std::cout << "Running DC..." << std::endl;
-        std::cout << "Constructing the index structure..." << std::endl;
-        unsigned long long index_construction_start_time = currentTime();
-        DCIndex *Index = new DCIndex(Graph);
-        unsigned long long index_construction_end_time = currentTime();
-        std::cout << "Index construction completed in " << timeFormatting(index_construction_end_time - index_construction_start_time).str() << std::endl;
-        delete Graph;
-        std::cout << "Solving queries..." << std::endl;
-        unsigned long long query_start_time = currentTime();
-        DC(Index, vertex_num, argv[2], argv[3]);
-        unsigned long long query_end_time = currentTime();
-        std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
-        std::cout << "DC completed!" << std::endl;
-    }
+    // if (std::strcmp(argv[argc - 1], "DC") == 0) {
+    //     std::cout << "Running DC..." << std::endl;
+    //     std::cout << "Constructing the index structure..." << std::endl;
+    //     unsigned long long index_construction_start_time = currentTime();
+    //     DCIndex *Index = new DCIndex(Graph);
+    //     unsigned long long index_construction_end_time = currentTime();
+    //     std::cout << "Index construction completed in " << timeFormatting(index_construction_end_time - index_construction_start_time).str() << std::endl;
+    //     delete Graph;
+    //     std::cout << "Solving queries..." << std::endl;
+    //     unsigned long long query_start_time = currentTime();
+    //     DC(Index, vertex_num, argv[2], argv[3]);
+    //     unsigned long long query_end_time = currentTime();
+    //     std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+    //     std::cout << "DC completed!" << std::endl;
+    // }
 
-    if (std::strcmp(argv[4], "RES") == 0) {
+    if (std::strcmp(argv[argc - 1], "RES") == 0) {
         std::cout << "Running optimized..." << std::endl;
         std::cout << "Constructing the index structure..." << std::endl;
         unsigned long long index_construction_start_time = currentTime();
@@ -83,11 +86,13 @@ int main(int argc, char * argv[]) {
         std::cout << "Index construction completed in " << timeFormatting(index_construction_end_time - index_construction_start_time).str() << std::endl;
         std::cout << "Index cost " << Index->size() << " bytes" << std::endl;
         delete Graph;
-        std::cout << "Solving queries..." << std::endl;
-        unsigned long long query_start_time = currentTime();
-        optimized(Index, vertex_num, argv[2], argv[3]);
-        unsigned long long query_end_time = currentTime();
-        std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+        for (int i = 2; i < argc - 1; i++) {
+            std::cout << "Solving queries..." << std::endl;
+            unsigned long long query_start_time = currentTime();
+            optimized(Index, vertex_num, argv[i], argv[argc - 2]);
+            unsigned long long query_end_time = currentTime();
+            std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+        }
         std::cout << "Optimized completed!" << std::endl;
     }
     unsigned long long end_time = currentTime();
